581e748ab908a6e8b72124f7414c2ce03770e123
Robert Iver <robert.iver@gmail.com>
2016-02-12 16:08:32 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scbparishApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scbparishApp
+ */
+angular.module('scbparishApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
